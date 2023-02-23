63872b595d83698b04cb67958b9913d53eb3e015
MAE <dup0729@gmail.com>
2023-02-23 17:22:32 +0900

    テスト

---
+++ src/main/java/com/example/Kaitai/KaitaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kaitai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaitaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaitaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kaitai/KaitaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kaitai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaitaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
